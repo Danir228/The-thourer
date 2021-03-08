c872ecd6c68d080be96fe0a47302432463b56c82
Данир Низамов <bezymniy228@gmail.com>
2021-03-08 23:44:35 +0300

    Сделал адаптив для 4 секций

--- src/components/buttons/appStore/appStore.js
+++ src/components/buttons/appStore/appStore.js
@@ -5,7 +5,7 @@ import apple from './apple.svg';
 import './appStore.css';
 
 const AppStoreButton = ({textButton, customIcon}) => {
-    const src = customIcon == undefined ? apple : customIcon;
+    const src = customIcon === undefined ? apple : customIcon;
     return (
         <>
             <button className="app-store_btn"><img src={src} alt={apple} /><span>{textButton}</span></button>
