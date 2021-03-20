3f16477a5f6896cd5606367d3528d6a0ce9ddecc
Данир Низамов <bezymniy228@gmail.com>
2021-03-21 01:35:04 +0300

    Сделал переключение классов активности, исправил баги в адаптивеБ подкорректировал бургер меню

--- src/components/main/components/section-4/section-4.js
+++ src/components/main/components/section-4/section-4.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {Col, Row} from 'reactstrap';
 import SectionForItem from './components/section-4-item';
 import SectionForMobItem from './components/section-4-mob-item';
@@ -8,64 +8,106 @@ import device from './Device.png';
 
 import './section-4.css';
 
-function SectionFor() {
-    return (
-        <>
-            <Row>
-                <Col className="section-4_title">
-                    <h1 className="section-4_title_item-1">Как это работает? <span className="textupdate">Попробуйте</span></h1>
-                    <h2 className="section-4_title_item-2"><span className="textupdate">Нажимайте на шаги</span>, чтобы увидеть, как работает приложение</h2>
-                </Col>
-            </Row>
-            <Row className="section-4_item">
-                <Col>
-                    <Row className="flex-column text-right">
-                        <Col className="top_item">
-                            <SectionForItem
-                                number={`<span class="num_update">1</span>`}
-                                title={'Построение планировок'}
-                                text={'Составляйте планировку помещения с помощью камеры вашего устройства'}/>
-                        </Col>
-                        <Col className="bottom_item">
-                            <SectionForItem
-                                number={`<span class="num_update">3</span>`}
-                                title={'Создание виртуальных туров'}
-                                text={'Собирайте созданные материалы в единый виртуальный тур'}/>
-                        </Col>
-                    </Row>
-                </Col>
-                <Col className="section-4_image">
-                    <img src={device} alt={device}/>
-                </Col>
-                <Col>
-                    <Row className="flex-column text-left">
-                        <Col className="top_item">
-                            <SectionForItem
-                                number={`<span class="num_update">2</span>`}
-                                title={'Съемка панорам'}
-                                text={'Передавайте цельную окружающую картину благодаря панораме 360° '}/>
-                        </Col>
-                        <Col className="bottom_item">
-                            <SectionForItem
-                                number={`<span class="num_update">4</span>`}
-                                title={'Делитесь созданными турами'}
-                                text={'Отправьте ссылку на виртуальный тур или вставьте проигрыватель на свой сайт'}/>
-                        </Col>
-                    </Row>
-                </Col>
-            </Row>
-            <Row className="section-4-mob_item">
-                <SectionForMobItem/>
-            </Row>
-            <Row className="section-4_bottom_item mx-0">
-                <Col className="section-4_bottom_item_body d-flex justify-content-around px-0 py-0">
-                    <p className="bottom_text">Создайте свой виртуальный тур <span className="bottom_text_item">прямо сейчас</span></p>
-                    <AppStoreButton
-                    textButton={'Скачать приложение'}/>
-                </Col>
-            </Row>
-        </>
-    )
-}
+export default class SectionFor extends Component {
+    state = {
+        first: true,
+        second: false,
+        third: false,
+        for: false
+    }
 
-export default SectionFor;
+    switchBtn = (btnType) => {
+        let firstNew = false,
+            secondNew = false,
+            thirdNew = false,
+            forNew = false;
+        switch(btnType) {
+            case 1:
+                firstNew = true;
+            break;
+            case 2:
+                secondNew = true;
+            break;
+            case 3:
+                thirdNew = true;
+            break;
+            case 4:
+                forNew = true;
+            break;
+            default: return;
+        }
+        this.setState({first: firstNew, second: secondNew , third: thirdNew, for: forNew});
+    }
+
+    render() {
+        return (
+            <>
+                <Row>
+                    <Col className="section-4_title">
+                        <h1 className="section-4_title_item-1">Как это работает? <span className="textupdate">Попробуйте</span></h1>
+                        <h2 className="section-4_title_item-2"><span className="textupdate">Нажимайте на шаги</span>, чтобы увидеть, как работает приложение</h2>
+                    </Col>
+                </Row>
+                <Row className="section-4_item">
+                    <Col>
+                        <Row className="flex-column text-right">
+                            <Col className="top_item">
+                                <SectionForItem
+                                    btnIndex={1}
+                                    func={this.switchBtn}
+                                    activeClass={this.state.first}
+                                    number={`<span class="num_update">1</span>`}
+                                    title={'Построение планировок'}
+                                    text={'Составляйте планировку помещения с помощью камеры вашего устройства'}/>
+                            </Col>
+                            <Col className="bottom_item">
+                                <SectionForItem
+                                    btnIndex={2}
+                                    func={this.switchBtn}
+                                    activeClass={this.state.second}
+                                    number={`<span class="num_update">3</span>`}
+                                    title={'Создание виртуальных туров'}
+                                    text={'Собирайте созданные материалы в единый виртуальный тур'}/>
+                            </Col>
+                        </Row>
+                    </Col>
+                    <Col className="section-4_image">
+                        <img src={device} alt={device}/>
+                    </Col>
+                    <Col>
+                        <Row className="flex-column text-left">
+                            <Col className="top_item">
+                                <SectionForItem
+                                    btnIndex={3}
+                                    func={this.switchBtn}
+                                    activeClass={this.state.third}
+                                    number={`<span class="num_update">2</span>`}
+                                    title={'Съемка панорам'}
+                                    text={'Передавайте цельную окружающую картину благодаря панораме 360° '}/>
+                            </Col>
+                            <Col className="bottom_item">
+                                <SectionForItem
+                                    btnIndex={4}
+                                    func={this.switchBtn}
+                                    activeClass={this.state.for}
+                                    number={`<span class="num_update">4</span>`}
+                                    title={'Делитесь созданными турами'}
+                                    text={'Отправьте ссылку на виртуальный тур или вставьте проигрыватель на свой сайт'}/>
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+                <Row className="section-4-mob_item">
+                    <SectionForMobItem/>
+                </Row>
+                <Row className="section-4_bottom_item mx-0">
+                    <Col className="section-4_bottom_item_body d-flex justify-content-around px-0 py-0">
+                        <p className="bottom_text">Создайте свой виртуальный тур <span className="bottom_text_item">прямо сейчас</span></p>
+                        <AppStoreButton
+                        textButton={'Скачать приложение'}/>
+                    </Col>
+                </Row>
+            </>
+        )
+    }
+}
--- src/components/main/components/section-6/components/section-6-when-item/section-6-when-item.js
+++ src/components/main/components/section-6/components/section-6-when-item/section-6-when-item.js
@@ -1,24 +1,78 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {Row} from 'reactstrap';
 
 import './section-6-when-item.css';
 
-export default function WhenItem({month, quarter, percent1, year, percent2 }) {
-    return (
-        <>
-            <Row className="whenitem">
-                <div className="whenitem_month">
-                    <div className="whenitem_style">{month}</div>
-                </div>
-                <div className="whenitem_quarter">
-                    <div className="whenitem_style">{quarter}</div>
-                    <div className="percent">{percent1}</div>
-                </div>
-                <div className="whenitem_year">
-                    <div className="whenitem_style">{year}</div>
-                    <div className="percent">{percent2}</div>
-                </div>
-            </Row>
-        </>
-    )
+export default class WhenItem extends Component {
+
+    state = {
+        month: false,
+        quarter: true,
+        year: false,
+    }
+
+    handleClickMonth = (e) => {
+        if (e.target.id === "1") {
+            this.setState({
+                month: true,
+                quarter: false,
+                year: false,
+            });
+        }
+    }
+
+    handleClickQuarter = (e) => {
+        if (e.target.id === "2") {
+            this.setState({
+                month: false,
+                quarter: true,
+                year: false,
+            });
+        }
+    }
+
+    handleClickYear = (e) => {
+        if (e.target.id === "3") {
+            this.setState({
+                month: false,
+                quarter: false,
+                year: true,
+            });
+        }
+    }
+
+    render() {
+        return (
+            <>
+                <Row className="whenitem">
+                    <div className="whenitem_month">
+                        <div
+                            id="1"
+                            className={this.state.month ? "whenitem_style whenitem_active" : "whenitem_style"}
+                            onClick={ (e) => this.handleClickMonth(e)}>
+                            {this.props.month}
+                        </div>
+                    </div>
+                    <div className="whenitem_quarter">
+                        <div
+                            id="2"
+                            className={this.state.quarter ? "whenitem_style whenitem_active" : "whenitem_style"}
+                            onClick={ (e) => this.handleClickQuarter(e)}>
+                            {this.props.quarter}
+                        </div>
+                        <div className="percent">{this.props.percent1}</div>
+                    </div>
+                    <div className="whenitem_year">
+                        <div
+                            id="3"
+                            className={this.state.year ? "whenitem_style whenitem_active" : "whenitem_style"}
+                            onClick={(e) => this.handleClickYear(e)}>
+                            {this.props.year}
+                        </div>
+                        <div className="percent">{this.props.percent2}</div>
+                    </div>
+                </Row>
+            </>
+        )
+    }
 }
